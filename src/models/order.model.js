08aab96507348b6b648d9f866a4e9d2b5385fcd3
user.name like : "Anuraj Satardekar <user.email like: "anusatardekar08@gmail.com>
2025-04-15 08:52:50 +0530

    daily scheduler completed

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,21 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import Dyecasting from "./dyecasting.model"
 
-// Sample part number to part name mapping
-const partDetails = {
-  "P001": "Part 1",
-  "P002": "Part 2",
-  "P003": "Part 3",
-  "P004": "Part 4",
-};
-
-//object for part number : casting name
-const castingDetails = {
-  "P001": "casting 1",
-  "P002": "casting 2",
-  "P003": "casting 3",
-  "P004": "casting 4"
-}
 
 // Define allowed part numbers for dropdown
 const allowedPartNumbers = Object.keys(partDetails);
